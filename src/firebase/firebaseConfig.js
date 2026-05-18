fb4157867c2ef480e9e0f877d6dd27925130e8ff
MEFS <240748569+TU4714315@users.noreply.github.com>
2026-05-18 05:53:16 -0700

    Add modular Firebase config

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDKUJxruoP7KjOl2UEH3Uw1nn0TqB6xnPQ",
+  authDomain: "m3tm-rased-07246627-7b0bf.firebaseapp.com",
+  projectId: "m3tm-rased-07246627-7b0bf",
+  storageBucket: "m3tm-rased-07246627-7b0bf.firebasestorage.app",
+  messagingSenderId: "1023709519997",
+  appId: "1:1023709519997:web:95b4059c2e9845d47ed114"
+};
+
+export const adminEmails = [
+  "moooom001@hotmail.com",
+  "mohammed.e.z.m2@gmail.com"
+];
